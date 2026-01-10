41c9cae88ce6e0da0835d50ee198fd9c126dd4c4
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-01-10 19:35:52 +0530

    explained models

--- core/home/models.py
+++ core/home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    #id = models.AutoField()
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    email = models.EmailField(null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
+
+
+class Car(models.Model):
+    car_name = models.CharField(max_length=50)
+    speed = models.IntegerField(default=50)
