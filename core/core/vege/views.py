31624bb26dafb00b1f1077c5963936f8694be744
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-01-15 14:02:30 +0530

    added new functions for delete and update

--- core/core/vege/views.py
+++ core/core/vege/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import *
+from django.http import HttpRequest
 
 # Create your views here.
 def recipes(request):
@@ -17,5 +18,38 @@ def recipes(request):
         )
 
         return redirect('/recipes/')
+    
+    queryset = Recipe.objects.all()
 
-    return render(request, 'recipes.html')
+    if request.GET.get('search'):
+        queryset =  queryset.filter(recipe_name__icontains = request.GET.get('search'))
+
+    context = {'recipes': queryset}
+
+    return render(request, 'recipes.html', context)
+
+def delete_recipe(request, id):
+    queryset = Recipe.objects.get(id = id)
+    queryset.delete()
+    return redirect('/recipes/')
+
+def update_recipe(request, id):
+    queryset = Recipe.objects.get(id = id)
+
+    if request.method == "POST":
+        data = request.POST
+        recipe_image = request.FILES['recipe_image']
+        recipe_name = data.get('recipe_name')
+        recipe_description = data.get('recipe_description')
+
+        queryset.recipe_description = recipe_description
+        queryset.recipe_name = recipe_name
+
+        if recipe_image:
+            queryset.recipe_image = recipe_image
+
+        queryset.save()    
+        return redirect('/recipes/')
+
+    context = {'recipe': queryset}
+    return render(request, 'update-recipe.html', context)
