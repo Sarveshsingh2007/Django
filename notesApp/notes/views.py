22b3e510cbaa655393555a9ed9fb64a079038f18
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-01-16 19:16:28 +0530

    created views functions like add_note, view_notes, delete_notes, download_notes

--- notesApp/notes/views.py
+++ notesApp/notes/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import FileResponse
+from notes.models import *
+import os
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def add_note(request):
+
+    if request.method == "POST":
+        Note.objects.create(
+            title = request.POST.get('title'),
+            description = request.POST.get('description'),  
+            file = request.FILES.get('file'),
+        )
+        return redirect('view_notes')
+
+
+
+    return render(request, "add.html")
+
+def view_notes(request):
+
+    notes = Note.objects.all().order_by('-created_at')
+    
+    context = {'notes': notes}
+    return render(request, "view.html", context)
+
+
+
+def delete_note(request, id):
+
+    note = get_object_or_404(Note, id=id)
+    note.delete()
+    return redirect('view_notes')
+
+
+
+def download_file(request, id):
+    note = get_object_or_404(Note, id=id)
+    return FileResponse(
+        note.file.open('rb'),
+        as_attachment=True,
+        filename=os.path.basename(note.file.name)
+    )
