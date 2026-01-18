2d79cf4017470a739ce046447f19bc087fbcca0f
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-01-18 18:45:08 +0530

    updated subject field

--- notesApp/notes/models.py
+++ notesApp/notes/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
-# Create your models here.
-
 class Note(models.Model):
-    subject = models.CharField(max_length=20)
+    subject = models.CharField(max_length=20,default="General")
     title = models.CharField(max_length=150)
     description = models.TextField()
-    file = models.FileField(upload_to='notes_files/')
+    file = models.FileField(upload_to='notes_files/',blank=True,null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
