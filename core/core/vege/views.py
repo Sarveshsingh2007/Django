b3d11600ba765cd151f0495651d595119af93791
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-01-20 19:57:57 +0530

    added new functions logout_page, login_page and register_page

--- core/core/vege/views.py
+++ core/core/vege/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from .models import *
 from django.http import HttpRequest
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def recipes(request):
@@ -53,3 +56,57 @@ def update_recipe(request, id):
 
     context = {'recipe': queryset}
     return render(request, 'update-recipe.html', context)
+
+def login_page(request):
+
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        if not User.objects.filter(username = username).exists():
+            messages.error(request, 'Invalid Username')
+            return redirect('/login/')
+        
+        user = authenticate(username = username, password = password)
+
+        if user is None:
+            messages.error(request, 'Invalid Password')
+            return redirect('/login/')
+        
+        else:
+            login(request, user)
+            return redirect('/recipes/')
+
+    return render(request, "login.html")
+
+
+def logout_page(request):
+    logout(request)
+    return redirect('/login/')
+
+def register_page(request):
+
+    if request.method == "POST":
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = User.objects.filter(username = username)
+        if user.exists():
+            messages.info(request, 'Already registerd.')
+            return redirect('/register/')
+
+        user = User.objects.create(
+            first_name = first_name,
+            last_name = last_name,
+            username = username,
+        )
+
+        user.set_password(password)
+        user.save()
+        messages.info(request, 'Account Created Successfully.')
+
+        return redirect('/register/')
+
+    return render(request, "register.html")
