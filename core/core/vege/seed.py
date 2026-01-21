685b531f91019784901aa66d480ca8af56e0282c
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-01-21 19:56:56 +0530

    created new file for fake data

---
+++ core/core/vege/seed.py
@@ -0,0 +1,38 @@
+from faker import Faker
+fake = Faker()
+import random
+from .models import *
+
+
+def seed_db(n=10) -> None:
+    try:
+        for i in range(n):
+
+            department = Department.objects.order_by('?').first()
+            if not department:
+                print("No departments found")
+                return
+
+            student_id_value = f'stu-{random.randint(100, 999)}'
+            student_name = fake.name()
+            student_email = fake.email()
+            student_age = random.randint(20, 30)
+            student_address = fake.address()
+
+            student_id_obj = StudentID.objects.create(
+                student_id=student_id_value
+            )
+
+            Student.objects.create(
+                department=department,   
+                student_id=student_id_obj,
+                student_name=student_name,
+                student_email=student_email,
+                student_age=student_age,
+                student_address=student_address,
+            )
+
+        print(f"{n} students seeded successfully ✅")
+
+    except Exception as e:
+        print("Error:", e)
