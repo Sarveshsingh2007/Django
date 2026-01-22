04ce3db85983dc7aad87a9822c238241d2f684b6
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-01-22 19:54:20 +0530

    added new model student

--- core/core/vege/models.py
+++ core/core/vege/models.py
@@ -26,6 +26,13 @@ class StudentID(models.Model):
     def __str__(self) -> str:
         return self.student_id
     
+
+class Subject(models.Model):
+    subject_name = models.CharField(max_length=100)    
+
+    def __str__(self):
+        return self.subject_name
+    
 class Student(models.Model):
     department = models.ForeignKey(Department, related_name="depart", on_delete=models.CASCADE)
     student_id = models.OneToOneField(StudentID, related_name="studentid", on_delete=models.CASCADE) 
@@ -41,4 +48,13 @@ class Student(models.Model):
         ordering = ['student_name']   
         verbose_name = "student" 
 
+class SubjectMarks(models.Model):
+    student = models.ForeignKey(Student, related_name="studentmarks", on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    marks = models.IntegerField()
 
+    def __str__(self) -> str:
+        return f"{self.student.student_name} - {self.subject.subject_name}"
+
+    class Meta:
+        unique_together = ['student', 'subject']
