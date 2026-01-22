88ea8fbefceaeaa3f5f13ffeba0151799bac720b
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-01-22 20:06:49 +0530

    added new function for get_students()

--- core/core/vege/views.py
+++ core/core/vege/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 
 @login_required(login_url="/login")
@@ -116,3 +117,13 @@ def register_page(request):
         return redirect('/register/')
 
     return render(request, "register.html")
+
+
+def get_students(request):
+    queryset = Student.objects.all()
+
+    paginator = Paginator(queryset, 25)
+    page_number = request.GET.get("page", 1)
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'report/students.html', {'queryset': page_obj})
