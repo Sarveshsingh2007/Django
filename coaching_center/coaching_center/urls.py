2a54180346e1d2cf8d810b4e6c8c282b81978cd2
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-01-31 21:32:55 +0530

    added new url for chatbot

--- coaching_center/coaching_center/urls.py
+++ coaching_center/coaching_center/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
+    path('chatbot/', include('chatbot.urls')), 
 ]
 
 # Serve media files in development
